31e9ff179ddd3cee0970bc598c3573b34ed78908
Selim Genc <selim.genc0620@gmail.com>
2025-11-19 15:08:17 +0100

    commit

---
+++ 02_JS_Strings/30 Aufgabe/aufgabe30.js
@@ -0,0 +1,10 @@
+let a = "bannana"
+let b = "apple" 
+
+
+let bannanaPricePerKilo = 2.24;
+
+console.log(bannanaPricePerKilo)
+console.   log("get result:", totalPrice.toFixed(2))
+
+
---
+++ 02_JS_Strings/30_index.js
@@ -0,0 +1,15 @@
+
+const bananaName = "banana";
+const appleName = "apple";
+
+const bananaPricePerKilo = 2.24;
+const applePricePerKilo = 3.45;
+const appleWeight = 0.34; // Kilo
+const bananaWeight = 0.22; // Kilo
+
+const totalPrice = (bananaPricePerKilo * bananaWeight) + (applePricePerKilo * appleWeight);
+console.log("get result:", totalPrice ."und"(2));
+
+const s = 2;
+const e = 4;
+console.log("get result", s, e);
