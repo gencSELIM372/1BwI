87f184f0b429c6009a5ca021232cffaddff8dbac
Selim Genc <selim.genc0620@gmail.com>
2026-01-14 16:26:17 +0100

    Task 03_Aufgabe

--- 60_Loops/03_Aufgabe.js
+++ 60_Loops/03_Aufgabe.js
@@ -1,9 +1,9 @@
 let summe = 0
 
-for ( let i = 20; i >= 1; i-- ) {
+for ( let i = 20; i >= 1; i-- ) {   
     console.log(i);
     summe = summe + i
 }   
 
 
-// wenn -- steht wird die zahl immer kleiner bis  zb.0 od 1
+// wenn -- steht wird die zahl immer kleiner bis  zb.0 od 1   summe = summe + i
